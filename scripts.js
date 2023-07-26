faa759c18bedaa9a127d1938a299d480e2b77cfe
marshall <113690263+marshallmhamad@users.noreply.github.com>
2023-07-26 09:28:26 +0300

    fixed the service cards

--- scripts.js
+++ scripts.js
@@ -50,38 +50,36 @@ var swiper = new Swiper(".slider-content", {
   },
 });
 
-/*==============================
-        About Tab List
-==============================*/
-const visionBtn = document.querySelector("[data-tab-vision]");
-const missionBtn = document.querySelector("[data-tab-mission]");
-const strategyBtn = document.querySelector("[data-tab-strategy]");
-const tabtext = document.querySelector("[data-tab-text]");
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-const visionBtnActive = function () {
-  visionBtn.classList.add("active");
-  missionBtn.classList.remove("active");
-  strategyBtn.classList.remove("active");
-  tabtext.innerHTML =
-    "Aliquam faucibus, odio nec commodo aliquam, neque felis placerat dui, a porta ante lectus dapibus est. Aliquam a bibendum mi, sed condimentum";
-};
-
-const missionBtnActive = function () {
-  missionBtn.classList.add("active");
-  visionBtn.classList.remove("active");
-  strategyBtn.classList.remove("active");
-  tabtext.innerHTML =
-    "Faucibus, odio nec commodo aliquam, neque felis placerat dui, a porta ante lectus dapibus est. Aliquam a bibendum mi,";
-};
-
-const strategyBtnActive = function () {
-  strategyBtn.classList.add("active");
-  visionBtn.classList.remove("active");
-  missionBtn.classList.remove("active");
-  tabtext.innerHTML =
-    "Odio nec commodo aliquam, neque felis placerat dui, a porta ante lectus dapibus est. Aliquam a bibendum mi, sed condimentum";
-};
-
-addEventOnElem(missionBtn, "click", missionBtnActive);
-addEventOnElem(visionBtn, "click", visionBtnActive);
-addEventOnElem(strategyBtn, "click", strategyBtnActive);
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },
+});
